8d8847d18cc889c722ee89fe2f365f134fa6b2e5
Joseph Doba <josephdoba@gmail.com>
2023-08-16 16:24:31 -0700

    footer adjustments

--- app/javascript/navbar.js
+++ app/javascript/navbar.js
@@ -1,6 +1,6 @@
-document.addEventListener("DOMContentLoaded", function () {
-  console.log("DOM is loaded");
+console.log("Hello from navbar.js");
 
+document.addEventListener("DOMContentLoaded", function () {
   const hamburger = document.querySelector(".hamburger");
 
   if (hamburger) {
