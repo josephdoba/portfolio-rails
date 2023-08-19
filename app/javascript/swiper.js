e4e9b195cff684c3e592392bb64c0894a4ee4be7
Joseph Doba <josephdoba@gmail.com>
2023-08-19 13:39:19 -0700

    implement swiper design

--- app/javascript/swiper.js
+++ app/javascript/swiper.js
@@ -1,16 +1,4 @@
 const swiper = new Swiper(".swiper-container", {
-  // Optional parameters
-  direction: "horizontal",
-  loop: true, // Enable looping
-
-  // If you need pagination
-  pagination: {
-    el: ".swiper-pagination",
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+  effect: "cards",
+  grabCursor: true,
 });
