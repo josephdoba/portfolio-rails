ee743b853eca1e39840e4e73e8fb5891330a12bb
Joseph Doba <josephdoba@gmail.com>
2023-08-20 14:32:13 -0700

    implement carousel and content

--- app/javascript/swiper.js
+++ app/javascript/swiper.js
@@ -4,16 +4,16 @@
 
 document.addEventListener("DOMContentLoaded", function () {
   var swiper = new Swiper(".mySwiper", {
-    effect: "cube",
-    grabCursor: true,
-    cubeEffect: {
-      shadow: true,
-      slideShadows: true,
-      shadowOffset: 20,
-      shadowScale: 0.94,
-    },
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
     pagination: {
       el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
   });
 });
