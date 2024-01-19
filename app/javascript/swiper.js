c54b494c2cb760a1b0e437cf920d350c95e74dd5
Joseph Doba <josephdoba@gmail.com>
2024-01-19 13:47:49 -0800

    troubleshoot highlight scroll issue

--- app/javascript/swiper.js
+++ app/javascript/swiper.js
@@ -2,21 +2,21 @@
 //   effect: "cards",
 // });
 
-document.addEventListener("DOMContentLoaded", function () {
-  var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//   var swiper = new Swiper(".mySwiper", {
+//     slidesPerView: 1,
+//     spaceBetween: 30,
+//     loop: true,
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//   });
+// });
 
 // var swiper = new Swiper(".mySwiper", {
 //   effect: "coverflow",
